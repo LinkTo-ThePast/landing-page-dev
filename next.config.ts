6dc895e7783345fe061676d40d3c75232659f6ae
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2025-02-18 19:31:50 -0600

    fixing next js json

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/landing-page-dev",
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
